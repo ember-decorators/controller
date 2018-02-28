c895bb1210dde7e877782ac4005e200e15028e3e
Chris Krycho <chris@chriskrycho.com>
2018-02-27 19:55:55 -0700

    Support both param and non-param forms in types.

--- index.d.ts
+++ index.d.ts
@@ -17,7 +17,29 @@ import { Registry } from "@ember/controller";
  * @function
  * @param {String} [controllerName] - The name of the controller to inject. If not provided, the property name will be used
  */
-export function controller(): PropertyDecorator;
+export function service(target: any, key: any): any;
+/**
+ * Decorator that wraps `Ember.inject.controller`
+ *
+ * Injects a controller into a Controller as the decorated property
+ *
+ *  ```javascript
+ * import Controller from '@ember/controller';
+ * import { controller } from 'ember-decorators/controller';
+ *
+ * export default class IndexController extends Controller {
+ *   @controller() application;
+ * }
+ * ```
+ *
+ * @function
+ * @param {String} [controllerName] - The name of the controller to inject. If not provided, the property name will be used
+ */
+export function service(
+  target: any,
+  key: any,
+  descriptor: PropertyDescriptor
+): PropertyDescriptor;
 /**
  * Decorator that wraps `Ember.inject.controller`
  *
